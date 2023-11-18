12d8606fdb45a2fd571009e31aa7f0c84a776fcb
Andraž Adamič <ajo@robert.local>
2023-11-18 17:52:28 +0100

    create .gitignore in cocreate-node, db_host and db_pass in .env

--- cocreate-node/src/index.ts
+++ cocreate-node/src/index.ts
@@ -9,25 +9,25 @@ const port = process.env.NODE_PORT
 
 const pool = new Pool({
     user: 'cocreate',
-    host: '93.103.55.194',
+    host: process.env.DB_HOST,
     database: 'cocreate',
-    password: 'cocreate_denis1234',
+    password: process.env.DB_PASSWORD,
     port: 42069,
-})
+});
 
 app.get('/', (req: Request, res: Response) => {
     pool.query('SELECT * FROM test', (error: Error, results: any) => {
         if (error) throw error
         res.status(200).json(results.rows)
-    })
-})
+    });
+});
 
 app.post('/user', (req: Request, res: Response) => {
     pool.query('INSERT INTO test (name) VALUES ($1)', ['denis'], (error: Error, results: any) => {
         if (error) throw error
         res.status(201).send(`User added with ID: ${results.insertId}`)
-    })
-})
+    });
+});
 
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
