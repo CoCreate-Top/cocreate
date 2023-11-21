3f8fe68ddc480a804daf9687df56362a286c4a13
Andraž Adamič <ajo@robert.local>
2023-11-21 15:10:57 +0100

    hash passwords

--- cocreate-node/src/index.ts
+++ cocreate-node/src/index.ts
@@ -7,13 +7,14 @@ dotenv.config()
 const app: Express = express()
 const port = process.env.NODE_PORT
 
-//TODO: add port to .env file
+const bcrypt = require('bcrypt')
+
 const pool = new Pool({
     user: 'cocreate',
     host: process.env.DB_HOST,
     database: 'cocreate',
     password: process.env.DB_PASSWORD,
-    port: 42069,
+    port: process.env.DB_PORT,
 });
 
 app.use(express.json());
@@ -25,39 +26,66 @@ app.get('/', (req: Request, res: Response) => {
     });
 });
 
-//TODO: hashing passwords
 // signup endpoint - needs name, email and password in body
 app.post('/signup', (req: Request, res: Response) => {
     const { name, email, password } = req.body;
-    pool.query('INSERT INTO "Users" (name, email, password) VALUES ($1, $2, $3)', [name, email, password], (error: Error, results: any) => {
-        if (error) return res.status(400).send(error);
-        res.status(201).send(`User added with ID: ${results.insertId}`);
+
+    // generates salt with number rounds of hashing provided in .env
+    bcrypt.genSalt(process.env.SALT_ROUNDS, (err: Error, salt: string) => {
+        if (err) return res.status(400).send(err);
+
+        // hashes password with salt
+        bcrypt.hash(password, salt, (err: Error, hash: string) => {
+            if (err) return res.status(400).send(err);
+
+            //? password is hashed in the following format: $2b$10$b63K/D03WFBktWy552L5XuibmiD5SxCrKg9kHCqOYaZwxRjIg14u2
+            // $2b$ - hashing algorithm
+            // 10 - number of rounds of hashing
+            // b63K/D03WFBktWy552L5Xu - salt
+            // ibmiD5SxCrKg9kHCqOYaZwxRjIg14u2 - hashed password
+
+            pool.query('INSERT INTO "Users" (name, email, password) VALUES ($1, $2, $3)', [name, email, hash], (error: Error, results: any) => {
+                if (error) return res.status(400).send(error);
+                console.log(results);
+                res.status(201);
+            });
+        });
     });
 });
 
-//TODO: hashing passwords and creating access tokens, refresh tokens
+//TODO: creating access tokens, refresh tokens
 // login endpoint - needs email and password in body
 app.post('/login', (req: Request, res: Response) => {
     const { email, password } = req.body;
-    pool.query('SELECT * FROM "Users" WHERE email = $1 AND password = $2', [email, password], (error: Error, results: any) => {
+    
+    // gets user by email (email is unique)
+    pool.query('SELECT * FROM "Users" WHERE email = $1', [email], (error: Error, results: any) => {
         if (error) res.status(400).send(error);
         if (results.rows.length == 0) {
             res.status(401).send('Invalid credentials')
         } else {
-            res.status(200).send('Login successful')
+            // if user exists compare password
+            bcrypt.compare(password, results.rows[0].password, (err: Error, result: boolean) => {
+                if (err) return res.status(400).send(err);
+                if (result) {
+                    res.status(200).send('Login successful')
+                } else {
+                    res.status(401).send('Invalid credentials')
+                }
+            });
         }
     });
 });
 
-//TODO: create a logout endpoint, which invalidates the refresh token
+//TODO: create a logout endpoint, which invalidates the refresh token and access token
 app.post('/logout', (req: Request, res: Response) => {
     
 });
 
-app.post('/user', (req: Request, res: Response) => {
-    pool.query('INSERT INTO test (name) VALUES ($1)', ['denis'], (error: Error, results: any) => {
+app.get('/users', (req: Request, res: Response) => {
+    pool.query('SELECT * FROM "Users"', (error: Error, results: any) => {
         if (error) throw error
-        res.status(201).send(`User added with ID: ${results.insertId}`)
+        res.status(201).send(results.rows)
     });
 });
 
