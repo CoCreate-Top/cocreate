eb6bdcf69c28e3638a93efb493faf17ea46ee1bf
Tim Rekelj <tim.rekelj@proton.me>
2023-11-28 00:48:05 +0100

    added daisy ui, started working on frontend

--- cocreate-angular/tailwind.config.js
+++ cocreate-angular/tailwind.config.js
@@ -3,9 +3,22 @@ module.exports = {
   content: [
     "./src/**/*.{html,js,ts}",
   ],
-  theme: {
-    extend: {},
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        cocreatetheme: {
+          "primary": "#3985B0",
+          "secondary": "#343434",
+          "accent": "#F1F0EA",
+          "neutral": "#F1F0EA",
+          "base-100": "#F1F0EA",
+          "info": "#F1F0EA",
+          "success": "#A8C69F",
+          "warning": "#FDBA74",
+          "error": "#F87171",
+        },
+      }
+    ],
   },
-  plugins: [],
-}
-
+};
