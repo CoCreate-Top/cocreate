69b78766d94ecfa507fa1c918f77294b26e4264a
Matija Ječnik <matija.jecnik@gmail.com>
2023-12-13 16:50:46 +0100

    add endpoint for testing API connection

--- cocreate-node/src/index.ts
+++ cocreate-node/src/index.ts
@@ -10,6 +10,10 @@ const port = process.env.NODE_PORT || 8000
 app.use(express.json());
 app.use('/api/auth', userAuthRoutes);
 
+app.get("/ping", (req, res) => {
+    res.status(200).json({ message: "pong" });
+});
+
 // app.get('/', (req: Request, res: Response) => {
 //     pool.query('SELECT * FROM "Users"', (error: Error, results: any) => {
 //         if (error) return res.status(400).send(error);
