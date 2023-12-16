4d73382b79999225e47ca1dee4a9ce749428ef32
Matija Ječnik <matija.jecnik@gmail.com>
2023-12-17 00:29:06 +0100

    Add cors package and types

--- cocreate-node/src/index.ts
+++ cocreate-node/src/index.ts
@@ -1,17 +1,18 @@
-import express, { Express } from 'express'
+import express, { Express } from 'express';
 import * as swaggerUi from 'swagger-ui-express';
-import session from 'express-session'
-import connectPgSimple from 'connect-pg-simple'
+import session from 'express-session';
+import connectPgSimple from 'connect-pg-simple';
 
-import dotenv from 'dotenv'
-import userAuthRoutes from './routes/userAuthRoutes'
-import apiRoutes from './routes/apiRoutes'
-import { ensureAuthenticated } from './controllers/sessionsController'
+import dotenv from 'dotenv';
+import userAuthRoutes from './routes/userAuthRoutes';
+import apiRoutes from './routes/apiRoutes';
+import { ensureAuthenticated } from './controllers/sessionsController';
 import pool from './config/database';
+import cors from 'cors';
 
-dotenv.config()
+dotenv.config();
 
-const app: Express = express()
+const app: Express = express();
 
 const PgStore = connectPgSimple(session);
 
@@ -28,8 +29,9 @@ app.use(session({
 // Swagger configuration
 const swaggerDocument = require('../swagger/swagger-output.json');
 
-const port = process.env.NODE_PORT || 8000
+const port = process.env.NODE_PORT || 8000;
 
+app.use(cors());
 app.use(express.json());
 app.use('/api/auth', userAuthRoutes);
 app.use('/api/db', ensureAuthenticated, apiRoutes);
@@ -44,21 +46,6 @@ app.get("/ping", (req, res) => {
 app.get("/ping/secure", ensureAuthenticated, (req, res) => {
     res.status(200).json({ message: "pong" });
 });
-
-
-// app.get('/', (req: Request, res: Response) => {
-//     pool.query('SELECT * FROM "users"', (error: Error, results: any) => {
-//         if (error) return res.status(400).send(error);
-//         res.status(200).json(results.rows)
-//     });
-// });
-
-// app.get('/users', (req: Request, res: Response) => {
-//     pool.query('SELECT * FROM "users"', (error: Error, results: any) => {
-//         if (error) throw error
-//         res.status(201).send(results.rows)
-//     });
-// });
 
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
