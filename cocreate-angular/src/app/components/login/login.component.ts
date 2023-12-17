149e40051701b3f9f79a4b22612291c23e6a03b3
Luka Žohar <luka.zohi@gmail.com>
2023-12-17 14:11:54 +0100

    Funkcije za login

--- cocreate-angular/src/app/components/login/login.component.ts
+++ cocreate-angular/src/app/components/login/login.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+
+  constructor(private authenticationService: AuthenticationService) {
+
+  }
+
+  login(username: string, password: string) {
+  }
+
+  loginGoogle() {
+  }
+
+  register(firstName: string, lastName: string, email: string, password: string) {
+  }
 
 }
---
+++ cocreate-angular/src/app/services/authentication.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+
+  constructor() { }
+
+  loginUsername(username: string, password: string) {
+    
+  }
+
+  loginGoogle() {
+
+  }
+
+  register() {
+
+  }
+
+}
