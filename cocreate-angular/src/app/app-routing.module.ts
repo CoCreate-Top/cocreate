9bdfcee1b1649751d32022cdcf259c1a82acb6fa
Luka Žohar <luka.zohi@gmail.com>
2023-12-17 13:02:51 +0100

    Dodan modul "Projects"

--- cocreate-angular/src/app/app-routing.module.ts
+++ cocreate-angular/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: 'login', component: LoginComponent },
+  // { path: 'register', component: RegisterComponent },
+  {
+    path: 'projects',
+    loadChildren: () =>
+      import('./modules/project/projects.module').then(m => m.ProjectsModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
