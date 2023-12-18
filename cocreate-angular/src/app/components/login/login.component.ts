f65a0f563a697fe6c3ce19f30e7944cfa3264a86
Ambrož Klančar <ambro.klancar@gmail.com>
2023-12-18 14:02:50 +0100

    reactive forms

--- cocreate-angular/src/app/components/login/login.component.ts
+++ cocreate-angular/src/app/components/login/login.component.ts
@@ -1,15 +1,25 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthenticationService } from 'src/app/services/authentication.service';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  profileForm = new FormGroup({
+    userName: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.profileForm.value);
+  }
 
   constructor(private authenticationService: AuthenticationService) {
 
--- cocreate-angular/src/app/components/register/register.component.ts
+++ cocreate-angular/src/app/components/register/register.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+  registerForm = new FormGroup({
+    userName: new FormControl(''),
+    password: new FormControl(''),
+    rpassword: new FormControl(''),
+  });
+
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.registerForm.value);
+  }
 
 }
