ad05a119abe8e87600bc74e536cf0282dfd6a0ca
Luka Žohar <luka.zohi@gmail.com>
2023-12-20 09:16:23 +0100

    Importan HTTPClient module & small fix

--- cocreate-angular/src/app/app.module.ts
+++ cocreate-angular/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { RegisterComponent } from './components/register/register.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     LoginComponent,
     RegisterComponent
   ],
--- cocreate-angular/src/app/components/register/register.component.ts
+++ cocreate-angular/src/app/components/register/register.component.ts
@@ -6,7 +6,7 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
 
