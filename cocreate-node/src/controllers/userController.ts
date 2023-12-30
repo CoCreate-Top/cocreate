a29363760c43717ba115b2567cbfbcc2b66f43a1
Matija Ječnik <matija.jecnik@gmail.com>
2023-12-30 15:05:31 +0100

    Fix error handling and return JSON responses

--- cocreate-node/src/controllers/userController.ts
+++ cocreate-node/src/controllers/userController.ts
@@ -9,7 +9,7 @@ export const getUser = (req: Request, res: Response) => {
             'SELECT * FROM "users" WHERE id = $1',
             [req.session.userId],
             (error: Error, results: any) => {
-                if (error) return res.status(400).send(error);
+                if (error) return res.status(400).json({ error });
                 const user: User = results.rows[0];
                 res.status(200).json({
                     id: user.id,
@@ -19,36 +19,36 @@ export const getUser = (req: Request, res: Response) => {
             }
         );
     } else {
-        return res.status(401).send("No user logged in");
+        return res.status(401).json({ error: "No user logged in" });
     }
 }
 
 export const changePassword = (req: Request, res: Response) => {
     const { new_password, old_password } = req.body;
-    if (!req.session.userId) return res.status(401).send("No user logged in");
+    if (!req.session.userId) return res.status(401).json({ error: "No user logged in" });
 
     pool.query(
         'SELECT * FROM "users" WHERE id = $1', 
         [req.session.userId], (error: Error, results: any) => {
-            if (error) return res.status(400).send(error);
-            if (results.rows.length == 0) return res.status(401).send('User not found');
+            if (error) return res.status(400).json({ error });
+            if (results.rows.length == 0) return res.status(401).json({error: 'User not found'});
             const user: User = results.rows[0];
 
             bcrypt.compare(old_password, user.password, (err: Error | undefined, result: boolean) => {
-            if (err) return res.status(400).send(err);
-            if (!result) return res.status(401).send('Invalid password');
+            if (err) return res.status(400).json({ err });
+            if (!result) return res.status(401).json({error: 'Invalid password'});
 
             bcrypt.genSalt(Number(process.env.SALT_ROUNDS), (err: Error | undefined, salt: string) => {
-                if (err) return res.status(400).send(err);
+                if (err) return res.status(400).json({ err });
 
                 bcrypt.hash(new_password, salt, (err: Error | undefined, hash: string) => {
-                    if (err) return res.status(400).send(err);
+                    if (err) return res.status(400).json({ err });
                     pool.query(
                         'UPDATE "users" SET password = $1 WHERE id = $2',
                         [hash, req.session.userId],
                         (error: Error) => {
-                            if (error) return res.status(400).send(error);
-                            res.status(200).send("Password updated");
+                            if (error) return res.status(400).json({ error });
+                            res.status(200).json({ message: "Password updated" });
                         }
                     );
                 });
@@ -58,12 +58,12 @@ export const changePassword = (req: Request, res: Response) => {
 }
 
 export const getMyProfile = (req: Request, res: Response) => {
-    if (!req.session.userId) return res.status(401).send("No user logged in");
+    if (!req.session.userId) return res.status(401).json({ error: "No user logged in" });
     pool.query(
         'SELECT * FROM "user_profile" WHERE id = $1',
         [req.session.userId],
         (error: Error, results: any) => {
-            if (error) return res.status(400).send(error);
+            if (error) return res.status(400).json({ error });
             const user: UserProfile = results.rows[0];
             res.status(200).json({
                 id: user.id,
@@ -89,7 +89,7 @@ export const getUserProfile = (req: Request, res: Response) => {
         'SELECT * FROM "user_profile" WHERE id = $1',
         [id],
         (error: Error, results: any) => {
-            if (error) return res.status(400).send(error);
+            if (error) return res.status(400).json({ error });
             const user: UserProfile = results.rows[0];
             res.status(200).json({
                 id: user.id,
@@ -128,7 +128,7 @@ export const changeUserProfile = (req: Request, res: Response) => {
     params.push(id);
 
     pool.query(query, params, (error: Error, results) => {
-        if (error) return res.status(400).send(error);
+        if (error) return res.status(400).json({ error });
         res.status(200).json(results.rows[0]);
     });
 }
