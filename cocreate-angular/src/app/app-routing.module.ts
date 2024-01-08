52fb02b0852cace6fe2b5c745ea6152229ad77b3
Ambrož Klančar <ambro.klancar@gmail.com>
2024-01-08 15:54:51 +0100

    dodan nav bar z linki in profile page

--- cocreate-angular/src/app/app-routing.module.ts
+++ cocreate-angular/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { AddProjectComponent } from './components/add-project/add-project.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent, pathMatch: 'full' },
   { path: 'register', component: RegisterComponent, pathMatch: 'full' },
   { path: 'add-project', component: AddProjectComponent, pathMatch: 'full' },
+  { path: 'profile', component: ProfileComponent, pathMatch: 'full' },
   { path: '',
     redirectTo: '/register',
     pathMatch: 'full'
--- cocreate-angular/src/app/app.module.ts
+++ cocreate-angular/src/app/app.module.ts
@@ -6,17 +6,20 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    ProfileComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
