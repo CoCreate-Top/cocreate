36732a117c3ac275e6f0ce749b35253afc6c63e9
Luka Žohar <luka.zohi@gmail.com>
2024-01-16 22:55:58 +0100

    Header iz ene komponente

--- cocreate-angular/src/app/components/header/header.component.ts
+++ cocreate-angular/src/app/components/header/header.component.ts
@@ -6,7 +6,7 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
 
