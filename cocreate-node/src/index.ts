d96c6709687ef73c10f6d75fbb5ad9bc71764983
Matija Ječnik <matija.jecnik@gmail.com>
2024-01-16 22:37:49 +0100

    secure: false

--- cocreate-node/src/index.ts
+++ cocreate-node/src/index.ts
@@ -24,7 +24,7 @@ app.use(session({
     secret: process.env.ACCESS_TOKEN_SECRET as string,
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: true, maxAge: 1000 * 60 * 30, sameSite: 'lax' }
+    cookie: { secure: false, maxAge: 1000 * 60 * 30, sameSite: 'lax' }
 }));
 
 // Swagger configuration
