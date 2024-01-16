b24eca65c9025c8d7f1e0207183f83a25120643e
Luka Žohar <luka.zohi@gmail.com>
2024-01-17 00:55:17 +0100

    Model za profil

---
+++ cocreate-angular/src/app/models/profile.ts
@@ -0,0 +1,10 @@
+export interface Profile {
+    id: string,
+    firstname: string,
+    lastname: string,
+    expirience: number,
+    phone: string,
+    email: string,
+    location: string,
+    socialMediaLinks: string,
+}
