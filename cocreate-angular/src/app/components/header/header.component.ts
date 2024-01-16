6112003f834abf900afa7c7427d0d50c744b22ef
Luka Žohar <luka.zohi@gmail.com>
2024-01-16 23:10:34 +0100

    Prikaz headerja samo ko je loginano

--- cocreate-angular/src/app/components/header/header.component.ts
+++ cocreate-angular/src/app/components/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -10,4 +12,23 @@ import { CommonModule } from '@angular/common';
 })
 export class HeaderComponent {
 
+  constructor(private authenticationService: AuthenticationService, private router: Router) {}
+
+  logout() {
+    this.authenticationService.logout().subscribe({
+      next: (res) => {
+        console.log(res);
+        this.router.navigate(["login"]);
+      },
+      error: (err) => { console.log(err) },
+    })
+  }
+
+  isLoggedIn(): boolean {
+    // TODO: hotfix
+    if (this.router.url === "/login" || this.router.url === "/register") {
+      return false;
+    }
+    return true;
+  }
 }
