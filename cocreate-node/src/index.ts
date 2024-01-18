7e06f758a228512240a151b1f470bd58acf5f268
Luka Žohar <luka.zohi@gmail.com>
2024-01-18 22:54:37 +0100

    Removed 'lax'

--- cocreate-node/src/index.ts
+++ cocreate-node/src/index.ts
@@ -24,7 +24,7 @@ app.use(session({
     secret: process.env.ACCESS_TOKEN_SECRET as string,
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: false, maxAge: 1000 * 60 * 30, sameSite: isProduction() ? 'lax' : false }
+    cookie: { secure: false, maxAge: 1000 * 60 * 30 }
 }));
 
 // Swagger configuration
